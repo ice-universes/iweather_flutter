548faab984657450b2df180457806317c6d6c271
ARCTURUS <1638330246@qq.com>
2022-05-04 22:25:31 +0800

    feat: [类] 坐标类添加地址和城市信息

--- lib/utils/location/coord.dart
+++ lib/utils/location/coord.dart
@@ -2,6 +2,13 @@
 class Coord {
   late num latitude;
   late num longitude;
+  late String? address; // 坐标详细地址
+  late String? city; // 坐标城市
 
-  Coord({required this.latitude, required this.longitude});
+  Coord({
+    required this.latitude,
+    required this.longitude,
+    this.address,
+    this.city,
+  });
 }
