07f78aff304211c3733c53c3b1bc08ac933bc57d
ARCTURUS <1638330246@qq.com>
2022-05-05 18:11:36 +0800

    feat: [类] 天气预警类

---
+++ lib/utils/weather/models/warings.dart
@@ -0,0 +1,21 @@
+class WaringItem {
+  late List<String>? type; // 预警类型
+  late String? sender; // 预警发布单位'
+  late String? event; // 事件名称
+  late DateTime? start;
+  late DateTime? end;
+  late String? description; // 预警内容
+
+  WaringItem({
+    this.type,
+    this.sender,
+    this.event,
+    int? start,
+    int? end,
+    this.description,
+  }) {
+    this.start =
+        start == null ? null : DateTime.fromMillisecondsSinceEpoch(start);
+    this.end = end == null ? null : DateTime.fromMillisecondsSinceEpoch(end);
+  }
+}
