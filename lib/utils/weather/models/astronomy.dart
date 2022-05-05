f5a4b746f33f090496459b4ab165feb1316f9f23
ARCTURUS <1638330246@qq.com>
2022-05-05 18:11:13 +0800

    feat: [类] 日月升落类

---
+++ lib/utils/weather/models/astronomy.dart
@@ -0,0 +1,51 @@
+import 'package:intl/intl.dart';
+
+class Sun {
+  late DateTime? _sunRise;
+  late DateTime? _sunSet;
+
+  Sun({int? sr, int? ss}) {
+    _sunRise = sr == null ? null : DateTime.fromMillisecondsSinceEpoch(sr);
+    _sunSet = ss == null ? null : DateTime.fromMillisecondsSinceEpoch(ss);
+  }
+
+  String get sunRise {
+    return _sunRise == null ? '' : DateFormat('HH:mm').format(_sunRise!);
+  }
+
+  String get sunSet {
+    return _sunSet == null ? '' : DateFormat('HH:mm').format(_sunSet!);
+  }
+
+  @override
+  toString() {
+    return ('# 日出时间: $sunRise\n'
+        '# 日落时间: $sunSet\n');
+  }
+}
+
+class Moon {
+  late DateTime? _moonRise;
+  late DateTime? _moonSet;
+  late num? moonPhase;
+
+  Moon({int? mr, int? ms, this.moonPhase}) {
+    _moonRise = mr == null ? null : DateTime.fromMillisecondsSinceEpoch(mr);
+    _moonSet = ms == null ? null : DateTime.fromMillisecondsSinceEpoch(ms);
+  }
+
+  String get moonRise {
+    return _moonRise == null ? '' : DateFormat('HH:mm').format(_moonRise!);
+  }
+
+  String get moonSet {
+    return _moonSet == null ? '' : DateFormat('HH:mm').format(_moonSet!);
+  }
+
+  @override
+  toString() {
+    return ('月出时间: $moonRise\n'
+        '月落时间: $moonSet\n'
+        '月相: $moonPhase\n');
+  }
+}
