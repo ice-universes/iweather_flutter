d0fbc578def3a6c8a653ffd9d0486a2a8edabd78
ARCTURUS <1638330246@qq.com>
2022-05-08 16:56:41 +0800

    feat: [组件] 搜索输入框

---
+++ lib/components/cities/search_textfield.dart
@@ -0,0 +1,45 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:iweather_flutter/store/controller.dart';
+
+// 城市搜索输入框
+class SearchTextField extends StatelessWidget {
+  final TextEditingController controller = TextEditingController();
+
+  final Controller c = Get.find<Controller>();
+
+  SearchTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      padding: const EdgeInsets.symmetric(
+        vertical: 5,
+        horizontal: 10,
+      ),
+      decoration: BoxDecoration(
+        color: const Color(0xfff5f5f5),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        controller: controller,
+        textAlignVertical: TextAlignVertical.bottom,
+        decoration: const InputDecoration(
+          prefixIcon: Icon(Icons.search),
+          prefixIconConstraints: BoxConstraints(),
+          hintText: '输入城市名称',
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+          ), // 去掉底部横线
+        ),
+        onSubmitted: (String value) {
+          // 回车
+        },
+        onChanged: (String value) {
+          // 输入改变
+        },
+      ),
+    );
+  }
+}
